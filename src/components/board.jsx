f8191168bcfbd064e11f5bacbcfda4ad7a0267d3
DiegoloM2 <diego.moreno.manes@hotmail.com>
2022-06-16 09:03:41 +0200

    phase 3 - tile component

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -9,12 +9,12 @@ class Board extends React.Component {
     render () {
         console.log(this.props.board.grid);
         return (
-            <div>
+            <div className = "board-grid">
                 {
                     this.props.board.grid.map(
                         (row, Ridx) => {
                             return (
-                                <div key = {`row${Ridx}`}>
+                                <div key = {`row${Ridx}`} className = "tile-row">
                                     {row.map((tile, Tidx) => (<Tile key = {`tile${Tidx}`} tile = {tile} updateGame = {this.props.updateGame}/>))}
                                 </div>
                             )
--- src/components/tile.jsx
+++ src/components/tile.jsx
@@ -3,12 +3,36 @@ import React from 'react';
 class Tile extends React.Component {
     constructor (props) {
         super(props);
+        this.handleClick = this.handleClick.bind(this);
 
     }
+
+    handleClick (e) {
+        // false = revealing, true = flagging
+        if (e.altKey) {
+            this.props.updateGame(this.props.tile, true)
+        }
+        else {
+            this.props.updateGame(this.props.tile, false); 
+        }
+
+    }
+
     render () {
-        return (
-            <span> Hello </span>
-        )
+        var tile = this.props.tile;
+        if (tile.explored) {
+            return <div className = "tile revealed"  onClick = {this.handleClick}>{tile.adjacentBombCount()}</div>
+    
+        }            
+        else if (tile.bombed) {
+            return <div className = "tile bombed" onClick = {this.handleClick}>{"\uD83D\uDCA3"}</div>
+        }
+        else if (tile.flagged) {
+            return <div className = "tile flagged" onClick = {this.handleClick}>&#128681</div>
+        }
+        else {
+            return <div className = "tile " onClick = {this.handleClick}> na </div>
+        }
     }
 }
 export default Tile;
